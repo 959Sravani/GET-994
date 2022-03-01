5de2db8c9e0b08cbaa86e31b168270f9764cdc90
959Sravani <100669399+959Sravani@users.noreply.github.com>
2022-03-01 16:10:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,10 @@ System.out.println("Hello world");
 int a=10;
 int b=5;
 int c=a+b;
-<<<<<<< HEAD
+
 String str="Hello";
-=======
+
 System.out.println(c);
 }
->>>>>>> 30ec0d093a680b70318ad534154ee18d1fb8f02a
+
 }
